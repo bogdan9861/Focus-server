e628a64d21be4a7406b452bb12a7c554dc5dae77
vebdev12 <ggkoudi@gmail.com>
2025-08-29 20:15:48 +0300

    rename app js

--- api/index.js
+++ api/index.js
@@ -49,10 +49,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/users", require("./routes/users"));
-app.use("/api/posts", require("./routes/posts"));
-app.use("/api/files", require("./routes/files"));
+app.use("/api/users", require("../routes/users"));
+app.use("/api/posts", require("../routes/posts"));
+app.use("/api/files", require("../routes/files"));
 
-app.use("/api/chats", require("./routes/chats"));
+app.use("/api/chats", require("../routes/chats"));
 
 module.exports = app;
