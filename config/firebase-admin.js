c98d126d9b0ce0f3cc6d3153b1b5f210cf2f494c
vebdev12 <ggkoudi@gmail.com>
2025-10-24 11:15:59 +0300

    add json settings

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,11 +1,9 @@
 var admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    clientEmail: process.env.client_email,
-    privateKey: process.env.private_key,
-    projectId: process.env.project_id,
-  }),
+  credential: admin.credential.cert(
+    "../focus-5ae89-firebase-adminsdk-fbsvc-27390319dc.json"
+  ),
 });
 
 module.exports = {
